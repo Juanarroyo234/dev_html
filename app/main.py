cb60d6114c388df6ba83b5deb202c8d15394a1bf
juan <jearroyo27@ucatolica.edu.co>
2025-05-15 08:07:06 -0500

    actualizaciones

--- app/main.py
+++ app/main.py
@@ -1,14 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
+from starlette.responses import HTMLResponse
 
 from app.routes import home
 
 app = FastAPI()
+
+router = APIRouter()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(home.router)
 
 templates = Jinja2Templates(directory="templates")
+
+@router.get ("/html", response_class= HTMLResponse)
+async def pure_html():
+    html_content="""
+    <html>
+    <head>
+        <title>Juan</title>
+    </head>
+    <body>
+        <h1>Mi html</h1>
+    </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content,status_code=200)
